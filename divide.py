6d37f8d20b08cc918a8499891c724aaf36187c1f
atulpatel991928 <atulpatelrsmt99@gmail.com>
2021-07-08 14:54:46 +0530

    first commit

--- divide.py
+++ divide.py
@@ -1,14 +1,2 @@
-
-def divisiblenum(array, n):
-	remainder = 0
-	for i in range (0, n):
-		remainder = ( remainder + array[i]) % 5
-	return (remainder == 0)
-
-
-array = [4,51, 90,23,42];
-n = 5
-if (divisiblenum(array, n)):
-	print("Yes")
-else:
-	print("No")
+mystr = ("hi i am atul hi hi skdfbmsvbsefms smbs zhi jfbjsm hi jsbfmsbsfbhi ahbshvisbf,sGfjam fhiikdbawjfv,jmbfahnhih dbhihihjhh hi fsfahi")
+print(mystr.count("hi"))
