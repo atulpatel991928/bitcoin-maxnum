386de7a230f708f1c29fc5a666c934f7d872bfb4
atulpatel991928 <atulpatelrsmt99@gmail.com>
2021-07-08 22:01:09 +0530

    dividenum

--- divide.py
+++ divide.py
@@ -1,2 +1,14 @@
-mystr = ("hi i am atul hi hi skdfbmsvbsefms smbs zhi jfbjsm hi jsbfmsbsfbhi ahbshvisbf,sGfjam fhiikdbawjfv,jmbfahnhih dbhihihjhh hi fsfahi")
-print(mystr.count("hi"))
+def isPossibleToMakeDivisible(arr, n):
+
+	remainder = 0
+	for i in range (0, n):
+		remainder = (remainder + arr[i]) % 5
+
+	return (remainder == 0)
+
+arr = [40, 50, 90,56,67 ];
+n = 3
+if (isPossibleToMakeDivisible(arr, n)):
+	print("Yes")
+else:
+	print("No")
